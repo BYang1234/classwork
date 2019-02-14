3952e80edf84403ce81af071e87eacb69fdb6fbf
BYang1234 <33939791+BYang1234@users.noreply.github.com>
2019-02-14 14:30:28 -0500

    Update drawing_from_sketch.py

--- drawing_from_sketch.py
+++ drawing_from_sketch.py
@@ -17,6 +17,7 @@ arcade.draw_circle_filled(540, 380, 50, arcade.color.YELLOW)
 arcade.draw_rectangle_filled(320, 50, 640, 100, arcade.color.GREEN)
 #First Tree
 arcade.draw_xywh_rectangle_filled(200, 50, 20, 75, arcade.color.BROWN)
+arcade.draw_triangle_filled(140, 150, 240, 150, 200, 200, arcade.color.DARK_GREEN)  
 #Second Tree
 arcade.draw_xywh_rectangle_filled(WIDTH - 200 - 40, 50, 20, 75, arcade.color.BROWN)
 #Third Tree
