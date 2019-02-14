e54ae40a1b0ec26757e8f41cb184dd59e8e1a80a
BYang1234 <33939791+BYang1234@users.noreply.github.com>
2019-02-14 14:27:08 -0500

    Add files via upload

---
+++ drawing_from_sketch.py
@@ -0,0 +1,29 @@
+import arcade
+
+
+WIDTH = 640
+HEIGHT = 480
+
+arcade.open_window(WIDTH, HEIGHT, "My Drawing")
+#Background
+arcade.set_background_color(arcade.color.SKY_BLUE)
+
+arcade.start_render()
+# Begin drawing
+
+#Sun
+arcade.draw_circle_filled(540, 380, 50, arcade.color.YELLOW)
+#BG
+arcade.draw_rectangle_filled(320, 50, 640, 100, arcade.color.GREEN)
+#First Tree
+arcade.draw_xywh_rectangle_filled(200, 50, 20, 75, arcade.color.BROWN)
+#Second Tree
+arcade.draw_xywh_rectangle_filled(WIDTH - 200 - 40, 50, 20, 75, arcade.color.BROWN)
+#Third Tree
+arcade.draw_xywh_rectangle_filled(515, 50, 20, 75, arcade.color.BROWN)
+
+
+
+# End drawing
+arcade.finish_render()
+arcade.run()
